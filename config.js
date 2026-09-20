ec9f9e568348813f11a056ebe13960a5a7ef5465
p52vtsd622-hash <p52vtsd622@privaterelay.appleid.com>
2026-09-20 20:50:49 +0330

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste your class server address between the quotes, for example:
 //   window.KALIMA_PROXY = "https://kalima.yourname.workers.dev";
 // Leave it empty ("") to keep using each person's own Gemini key.
-window.KALIMA_PROXY = "";
+window.KALIMA_PROXY = "https://kalima.wyp44cctp8.workers.dev/";
